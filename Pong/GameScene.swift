68857fb3df1ff29f912e94cc4226fac21769faa0
PandaZ3D <aaboytes@ucsc.edu>
2017-08-18 23:24:51 -0700

    added score that prints to console and ball position reset

--- Pong/GameScene.swift
+++ Pong/GameScene.swift
@@ -19,11 +19,13 @@ class GameScene: SKScene {
     
     override func didMove(to view: SKView) {
         
+        startGame()
+        
         ball = self.childNode(withName: "ball") as! SKSpriteNode
         bot = self.childNode(withName: "bot") as! SKSpriteNode
         usr = self.childNode(withName: "usr") as! SKSpriteNode
         
-        ball.physicsBody?.applyImpulse(CGVector(dx: -12, dy: 12))
+        ball.physicsBody?.applyImpulse(CGVector(dx: 12, dy: 12))
         
         let border = SKPhysicsBody(edgeLoopFrom: self.frame)
         
@@ -35,22 +37,47 @@ class GameScene: SKScene {
     }
     
     func startGame() {
+        //init scores
         score = [0,0]
     }
     
+    func addScore(player: SKSpriteNode) {
+        //remove forces on ball
+        ball.position = CGPoint(x: 0, y: 0)
+        ball.physicsBody?.applyImpulse(CGVector(dx: 0, dy: 0))
+        
+        //increment scores
+        if player == usr {
+            //increment usr score
+            score[0] += 1
+            //add impulse
+            ball.physicsBody?.applyImpulse(CGVector(dx: 12, dy: 12))
+        }
+        else if player == bot {
+            //imcrement score for bot
+            score[1] += 1
+            //add impulse
+            ball.physicsBody?.applyImpulse(CGVector(dx: -12, dy: 12))
+        }
+        
+        print(score)
+    }
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //every touch happening cont.
         for touch in touches {
             let location = touch.location(in: self)
-            
+            //track UI touch
             usr.run(SKAction.moveTo(x: location.x, duration: 0.2))
             
         }
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //every touch happening cont.
         for touch in touches {
             let location = touch.location(in: self)
-            
+            //move usr paddle along with UI touch
             usr.run(SKAction.moveTo(x: location.x, duration: 0.2))
             
         }
@@ -60,5 +87,16 @@ class GameScene: SKScene {
         // Called before each frame is rendered
         
         bot.run(SKAction.moveTo(x: ball.position.x, duration: 1))
+        
+        if ball.position.y <= usr.position.y - 70 {
+            //usr position lower half (-)
+            //add score to AI
+            addScore(player: bot)
+        }
+        else if ball.position.y >= bot.position.y + 70 {
+            //bot position upper half (+)
+            //add score to usr
+            addScore(player: usr)
+        }
     }
 }
